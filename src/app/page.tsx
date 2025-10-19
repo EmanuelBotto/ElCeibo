8f70e4cfad9e53ac29e370e0e03952a37eb9cfb7
Emanuel Botto <emanuelbotto25@gmail.com>
2025-10-19 03:33:33 -0300

    Arreglar imports faltantes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import React, { useState } from "react";
-import Caja from './ventana/caja';
-import Producto from './ventana/producto';
-import FichasClientes from './mascota/fichas-clientes';
+import Caja from './ventana/caja.jsx';
+import Producto from './ventana/producto.jsx';
+import FichasClientes from './mascota/fichas-clientes.jsx';
 import DashboardLayout from '@/components/DashboardLayout';
 import DashboardPage from './dashboard/page';
-import Item from './ventana/item';
-import Ingreso from './ventana/ingreso';
-import DistribuidoresDeudas from './ventana/distribuidores-deudas';
+import Item from './ventana/item.jsx';
+import Ingreso from './ventana/ingreso.jsx';
+import DistribuidoresDeudas from './ventana/distribuidores-deudas.jsx';
 // import ProtectedRoute from '@/components/ProtectedRoute';
 
 export default function HomePage() {
